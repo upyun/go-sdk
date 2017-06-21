91433af050965d135817fd91940054e77c573351
hongbo.mo <hongbo.mo@upai.com>
2017-06-21 03:19:27 +0000

    add testcase

--- upyun/purge_test.go
+++ upyun/purge_test.go
@@ -21,4 +21,11 @@ func TestPurge(t *testing.T) {
 	Nil(t, err)
 	Equal(t, len(fails), 1)
 	Equal(t, fails[0], fmt.Sprintf("http://%s-t.b0.upaiyun.com/demo.jpg", up.Bucket))
+
+	fails, err = up.Purge([]string{
+		fmt.Sprintf("http://%s.b0.upaiyun.com/测试.jpg", up.Bucket),
+	})
+
+	Nil(t, err)
+	Equal(t, len(fails), 0)
 }
--- upyun/upyun_test.go
+++ upyun/upyun_test.go
@@ -83,7 +83,7 @@ func isNil(object interface{}) bool {
 func TestMain(m *testing.M) {
 	_, err := up.Usage()
 	if err != nil {
-		fmt.Println("failed to login. Have set UPYUN_BUCKET UPYUN_USERNAME UPYUN_PASSWORD UPYUN_SECRET?")
+		fmt.Println("failed to login. Have set UPYUN_BUCKET UPYUN_USERNAME UPYUN_PASSWORD UPYUN_SECRET UPYUN_NOTIFY?")
 		os.Exit(-1)
 	}
 	clean := func() {
